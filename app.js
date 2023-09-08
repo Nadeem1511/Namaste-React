f0407751edda1ccc8696907dccf84f1b98cc36ac
Nadeem1511 <mohamednadeem1511@gmail.com>
2023-09-08 22:31:47 +0530

    Got to know about Component composition

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
- const heading = React.createElement("h1",{id:"head"},"Hello, Nadeem all the best to complete Namaste-React");
+ const Title = () => (
+    <div>
+        <h1>Hello, Nadeem all the best to complete Namaste-React</h1>
+    </div>
+ );
  
  const HeadingComponent = () => (
   <div>  
-    <h1 id="head">Hello, Nadeem all the best to complete Namaste React🚀</h1> 
+    <h1 id="head">Hello, Nadeem all the best to complete Namaste React🚀</h1>
+    <Title /> 
   </div>  
  );  
 
- const root = ReactDOM.createRoot(document.getElementById("root"));
- root.render(heading);
+//  const root = ReactDOM.createRoot(document.getElementById("root"));
+//  root.render(<Title />);
 
  const root2 = ReactDOM.createRoot(document.getElementById("root2"));
  root2.render(<HeadingComponent />);
