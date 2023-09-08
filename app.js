28c3da4237666e85bf387a3bd59407dc869ca594
Nadeem1511 <mohamednadeem1511@gmail.com>
2023-09-08 22:25:35 +0530

    Using Functional Component

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
- const parent = React.createElement(
-    "div", 
-    {id: "parent"},
-    React.createElement("div", {id: "child"}, [
-    React.createElement("h2", {},"Hello, I am a H2 tag!!"),
-    React.createElement("h3", {}, "Hello, I am a H3 tag!!")
-    ]),
-    React.createElement("div", {id: "child"}, [
-        React.createElement("h2", {},"Hello, I am a H2 tag!!"),
-        React.createElement("h3", {}, "Hello, I am a H3 tag!!")
-        ])
- );
-
- console.log(parent);
+ const heading = React.createElement("h1",{id:"head"},"Hello, Nadeem all the best to complete Namaste-React");
+ 
+ const HeadingComponent = () => (
+  <div>  
+    <h1 id="head">Hello, Nadeem all the best to complete Namaste React🚀</h1> 
+  </div>  
+ );  
 
  const root = ReactDOM.createRoot(document.getElementById("root"));
- root.render(parent);
+ root.render(heading);
+
+ const root2 = ReactDOM.createRoot(document.getElementById("root2"));
+ root2.render(<HeadingComponent />);
+ 
+
+
+ 
