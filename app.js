e52daf6a17f8656970da49d58428a7d5404c3718
Nadeem1511 <mohamednadeem1511@gmail.com>
2023-09-08 16:55:14 +0530

    epi-1

---
+++ app.js
@@ -0,0 +1,18 @@
+
+ const parent = React.createElement(
+    "div", 
+    {id: "parent"},
+    React.createElement("div", {id: "child"}, [
+    React.createElement("h2", {},"Hello, I am a H2 tag!!"),
+    React.createElement("h3", {}, "Hello, I am a H3 tag!!")
+    ]),
+    React.createElement("div", {id: "child"}, [
+        React.createElement("h2", {},"Hello, I am a H2 tag!!"),
+        React.createElement("h3", {}, "Hello, I am a H3 tag!!")
+        ])
+ );
+
+ console.log(parent);
+
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+ root.render(parent);
