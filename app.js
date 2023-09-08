2afbd0187f14e47304c67d79837515342e68412f
Nadeem1511 <mohamednadeem1511@gmail.com>
2023-09-08 23:04:29 +0530

    Played with components and elements

--- app.js
+++ app.js
@@ -6,16 +6,18 @@ import ReactDOM from "react-dom/client";
         <h1>Hello, Nadeem all the best to complete Namaste-React</h1>
     </div>
  );
- 
+
  const HeadingComponent = () => (
-  <div>  
+  <div> 
+     <Title />
+     <Title></Title>
+     {Title()}
     <h1 id="head">Hello, Nadeem all the best to complete Namaste React🚀</h1>
-    <Title /> 
   </div>  
  );  
 
-//  const root = ReactDOM.createRoot(document.getElementById("root"));
-//  root.render(<Title />);
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+ root.render(<HeadingComponent />);
 
  const root2 = ReactDOM.createRoot(document.getElementById("root2"));
  root2.render(<HeadingComponent />);
